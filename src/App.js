b7b4c8df1c14eb2b662363a16ac3d51a1a4e33c5
NataliaTymkiv <Natalia1987989@gmail.com>
2023-02-28 11:10:37 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 0000
         </a>
       </header>
     </div>
